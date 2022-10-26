855417c2e2c4d24183a32f8c504a2be74b22167b
carlos-rian <crian.rian@gmail.com>
2022-10-26 17:56:39 -0300

    test: update test_mock_repositorio.py

--- tests/test_mock_repositorio.py
+++ tests/test_mock_repositorio.py
@@ -14,12 +14,15 @@ def livro():
         "userId": 1,
         "id": 1,
         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
     }
 
 
+# o livro será passado automaticamente como parâmetro, pois é uma fixture
 def test_blog(livro: dict):
     mocker = Mock()
     mocker.return_value = livro
 
-    mocker(Blog.post_by_user_id, userId="1")
+    livro2 = Blog.post_by_user_id(userId="1")
+
+    assert mocker() == livro2
